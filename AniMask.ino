440b259c95cf8147ed316e1d680ef10ceeeaabca
Patrick Gillan <pgillan@minorimpact.com>
2021-03-13 16:50:34 -0500

    Updated to use wider matrix.

--- AniMask.ino
+++ AniMask.ino
@@ -1,10 +1,10 @@
 
 #define CEREAL
-#define RIGID
 
-#define MATRIX_W        8           /* Size (columns) of entire matrix */
-#define MATRIX_H        8           /* and rows */
-#define MATRIX          60
+#define MASK
+#define MATRIX_W        15           /* Size (columns) of entire matrix */
+#define MATRIX_H        6           /* and rows */
+#define MATRIX          6
 #define MAX_BRIGHT      255
 #define BLUETOOTH       
 
@@ -12,10 +12,11 @@
 #include <RFClickerLib.h> // https://github.com/pgillan145/RFClickerLib
 #include "AniMask.h"
  
+char clicker_history[HISTORY_LENGTH];
 
 void setup() {
   PGardLibSetup();
-  RFClickerSetup();
+  RFClickerSetup(clicker_history);
 
   matrix->begin();
   matrix->setBrightness(128);
@@ -29,13 +30,51 @@ float humidity = 0.0;
 int16_t speed = 60;
 int8_t color = 0;
 
-char clicker_history[HISTORY_LENGTH];
-
 uint32_t now = 0;
 
 void loop() {
   now = millis();
   doAnimations();  
+
+  
+  String input_str = serialInput(10);
+
+  if (input_str) {
+    char c = input_str.charAt(0);
+    String s2 = input_str.substring(1);
+    int value_int = s2.toInt();
+    switch(c) {
+      case 'w':
+#ifdef CEREAL
+        SPL("up");
+#endif
+        addAnimation(smile, sizeof(smile)/sizeof(smile[0]), 15, ANI_HOLD);
+        break;
+      case 'a':
+#ifdef CEREAL
+        SPL("left");
+#endif
+        addAnimation(blue, sizeof(blue)/sizeof(blue[0]), 15, ANI_REPEAT);
+        break;
+      case 's':
+#ifdef CEREAL
+        Serial.println("down");
+#endif
+        addAnimation(frown, sizeof(frown)/sizeof(frown[0]), 15, ANI_HOLD);
+        break;
+      case 'd':
+#ifdef CEREAL
+        SPL("right");
+#endif
+        addAnimation(red, sizeof(red)/sizeof(red[0]), 15, ANI_REPEAT);
+        break;
+      case '?':
+        SP("FOO:");
+        SPL(value_int);
+        break;
+    };
+  };
+
   
   getClickerHistory(clicker_history);
     for (uint8_t i = 0 ; i < HISTORY_LENGTH; i++) {
@@ -52,7 +91,7 @@ void loop() {
 #ifdef CEREAL
           Serial.println("down");
 #endif
-          addAnimation(frown, sizeof(frown)/sizeof(frown[0]),15, ANI_HOLD);
+          addAnimation(frown, sizeof(frown)/sizeof(frown[0]), 15, ANI_HOLD);
         }
         else {
         }
